c9ce1e269a89e1ece927ecb95a366e5059292885
skachkovkirill <skachkovkiril@yandex.ru>
2023-03-12 15:31:28 +0300

    Changed receiving and sending messages to JSON

--- python-fastapi-app/main.py
+++ python-fastapi-app/main.py
@@ -16,23 +16,23 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def broadcast(self, websocket: WebSocket, data: str):
+    async def broadcast(self, websocket: WebSocket, data: dict):
         for connection in self.active_connections:
             if not websocket is connection:
-                await connection.send_text(data=data)
+                await connection.send_json(data=data)
 
 
 manager = ConnectionManager()
 
 
-@app.websocket("/ws/{id}")
-async def websocket_endpoint(websocket: WebSocket, id: int):
+@app.websocket("/ws/{uuid}")
+async def websocket_endpoint(websocket: WebSocket, uuid: str):
     await manager.connect(websocket)
     try:
-        await manager.broadcast(websocket, f"Пользователь #{id} подключился к чату ({datetime.datetime.now()})")
+        await manager.broadcast(websocket, {"id": uuid, "dt": str(datetime.datetime.now())})
         while True:
-            data = await websocket.receive_text()
-            await manager.broadcast(websocket, data)
+            data = await websocket.receive_json()
+            await manager.broadcast(websocket, {"id": uuid, "dt": str(datetime.datetime.now()), "data": data})
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(websocket, f"Client #{websocket} disconnected")
+        await manager.broadcast(websocket, {"test": f"Client #{websocket} disconnected"})   
