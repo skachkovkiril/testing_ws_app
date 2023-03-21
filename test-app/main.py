e1b65fde89dd88c95c75084a0e3b942cecf06dfa
skachkovkirill <skachkovkiril@yandex.ru>
2023-03-21 14:12:24 +0300

    Added the ability to set the number of users

--- test-app/main.py
+++ test-app/main.py
@@ -2,5 +2,5 @@ from test_ws import Testing
 
 
 if __name__=="__main__":
-    testing_obj = Testing(count_clients=4)
+    testing_obj = Testing(count_clients=int(input()))
     testing_obj.start_with_stop_by_symbol()
