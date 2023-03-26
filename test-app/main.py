df83a810e0afcc02bb5df21f900a105571d51c92
skachkovkirill <skachkovkiril@yandex.ru>
2023-03-26 13:45:18 +0300

    Deleted old testing app

--- test-app/main.py
+++
@@ -1,9 +0,0 @@
-from test_ws import Testing
-
-
-if __name__=="__main__":
-    testing_obj = Testing(count_clients=int(input("Required number of users: ")))
-    testing_obj.start()
-    testing_obj.get_average_connection_speed()
-    testing_obj.stop_by_symbol()
-    testing_obj.get_average_disconnection_speed()
