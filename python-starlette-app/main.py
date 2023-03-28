2cef30382dd92245b5017b765f8fa7f0de996d73
skachkovkirill <skachkovkiril@yandex.ru>
2023-03-28 12:43:23 +0300

    Added Starlette ws-app

---
+++ python-starlette-app/main.py
@@ -0,0 +1,35 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+from starlette.websockets import WebSocket, WebSocketDisconnect
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, data: dict):
+        for connection in self.active_connections:
+            await connection.send_json(data=data)
+
+
+manager = ConnectionManager()
+
+app = Starlette("WebSocket Example")
+
+@app.websocket_route("/ws/{uuid}")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_json()
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        await manager.disconnect(websocket)
