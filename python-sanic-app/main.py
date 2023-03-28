fd4e7c4aec25430c9e50e8904be7b508d6cf745b
skachkovkirill <skachkovkiril@yandex.ru>
2023-03-28 12:05:25 +0300

    Added sanic ws-app

---
+++ python-sanic-app/main.py
@@ -0,0 +1,34 @@
+from sanic import Sanic
+from sanic import Request, Websocket
+import json
+
+app = Sanic("WebSocketExample")
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list = []
+
+    async def connect(self, websocket):
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, data: dict):
+        for connection in self.active_connections:
+            await connection.send(json.dumps(data))
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/<uuid>")
+async def feed(request: Request, ws: Websocket, uuid: str):
+    await manager.connect(ws)
+    try:
+        while True:
+            data = json.loads(await ws.recv())
+            await manager.broadcast(data)
+    except:
+        manager.disconnect(ws)
